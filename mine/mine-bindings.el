0a4cd460d73ba3f652063b81d53f49fb51bef3bc
Adam Shannon <adam.shannon@banno.com>
2014-10-03 09:56:40 -0500

    sbt-run.

--- mine/mine-bindings.el
+++ mine/mine-bindings.el
@@ -32,6 +32,7 @@
 (global-set-key (kbd "C-c p") 'doc-view-previous-page)
 
 ;; sbt-mode
+(global-set-key (kbd "C-c s r") 'sbt-run)
 (global-set-key (kbd "C-c s t") 'sbt-test)
 (global-set-key (kbd "C-c s c") 'sbt-compile)
 (global-set-key (kbd "C-c s o") 'sbt-test-only-current-test)
