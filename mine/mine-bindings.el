59c1b9a2ab9e976e5a185b6fa98a7120f83dc6e9
Adam Shannon <adam.shannon@banno.com>
2014-12-03 10:08:05 -0600

    adding better binding for org-priority

--- mine/mine-bindings.el
+++ mine/mine-bindings.el
@@ -15,6 +15,7 @@
 (global-set-key (kbd "C-c l") 'org-store-link)
 (global-set-key (kbd "C-c a") 'org-agenda)
 (global-set-key (kbd "C-c c") 'org-capture)
+(global-set-key (kbd "C-c p") 'org-priority)
 (global-set-key (kbd "C-x C-o") 'mine-org-switch-to-file)
 
 ;; frame/window
