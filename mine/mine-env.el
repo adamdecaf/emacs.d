23d6bffe2ada78dc9b39d99a1623bc62987f0a68
Adam Shannon <adam.shannon@banno.com>
2015-03-03 11:52:22 -0600

    set some env variable in a better way

--- mine/mine-env.el
+++ mine/mine-env.el
@@ -1,7 +1,16 @@
 ;; env steup
+(require 's)
 
 (when (memq window-system '(mac ns))
   (exec-path-from-shell-initialize))
+
+(defun mine-set-emacs-env-variable (name value)
+  (if (s-blank? (getenv name))
+      (setenv name value)))
+
+(defun mine-update-path (incoming)
+  (if (not (s-contains? (getenv "PATH") incoming))
+      (setenv (concat (getenv "PATH") ":" incoming))))
 
 ;; shell
 ;; (exec-path-from-shell-initialize "PATH") ;; done automatically on osx
@@ -23,6 +32,10 @@
 (setenv "DOCKER_CERT_PATH" "/Users/adam/.boot2docker/certs/boot2docker-vm")
 (setenv "DOCKER_TLS_VERIFY" "1")
 
+;; go
+(mine-set-emacs-env-variable "GOPATH" "/Users/adam/src/go")
+(mine-update-path (concat (getenv "GOPATH") "/bin"))
+
 (defun mine-show-docker-env-variables()
   (interactive)
   (message (concat "DOCKER_HOST" (getenv "DOCKER_HOST")))
@@ -33,5 +46,9 @@
   (interactive)
   (message (getenv "AWS_ACCESS_KEY_ID"))
   (message (concat "aws creds in use: " whose-aws-creds-are-in-use)))
+
+(defun mine-echo-path()
+  (interactive)
+  (message (getenv "PATH")))
 
 (provide 'mine-env)
