7749f5719d08036bc4687e77e909068607ae12f6
Adam Shannon <adam.shannon@banno.com>
2015-04-15 14:38:43 -0500

    trying to use git remote show in current dir

--- mine/mine-magit.el
+++ mine/mine-magit.el
@@ -4,11 +4,17 @@
   (interactive)
   (switch-to-buffer "*magit-process*"))
 
+(defun get-remote-name(&optional maybe-remote-name)
+  (if (not maybe-remote-name) "origin" maybe-remote-name))
+
+;; todo: How does magit get the current directory?
+;; Or is there a magit-shell-command method or something we can use?
 (defun magit/git-remote-show-origin(&optional maybe-remote-name)
   (interactive)
-  (let ((remote-name (if (not maybe-remote-name)
-                         "origin"
-                       maybe-remote-name)))
-    (async-shell-command (concat "git remote show " remote-name))))
+  (let* ((remote-name (get-remote-name maybe-remote-name))
+         (last-dir default-directory))
+    (cd default-directory)
+    (async-shell-command (concat "git remote show " remote-name))
+    (cd last-dir)))
 
 (provide 'mine-magit)
