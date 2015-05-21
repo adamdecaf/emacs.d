6cb2b151ca29e6ec2581c3e370c53827833a3615
Adam Shannon <adam@ashannon.us>
2015-05-21 17:45:59 -0500

    Remove bindings that I don't use.

--- mine/mine-bindings.el
+++ mine/mine-bindings.el
@@ -9,10 +9,8 @@
 ;; date/time
 (global-set-key (kbd "C-c d") 'insert-date)
 (global-set-key (kbd "C-c t") 'insert-iso-timestamp)
-(global-set-key (kbd "C-x t") 'mine-message-the-time)
 
 ;; org
-(global-set-key (kbd "C-c l") 'org-store-link)
 (global-set-key (kbd "C-c a") 'org-agenda)
 (global-set-key (kbd "C-c c") 'org-capture)
 (global-set-key (kbd "C-x C-o") 'mine-org-switch-to-file)
@@ -23,10 +21,6 @@
 (global-set-key (kbd "C-x 8") 'toggle-window-split)
 (global-set-key (kbd "C-x 9") 'toggle-window-split)
 
-;; buffers
-(global-set-key (kbd "C-c k") 'delete-this-buffer-and-file)
-(global-set-key (kbd "C-x C-r") 'rename-file-and-buffer)
-
 ;; text
 (global-set-key (kbd "C-x a r") 'align-regexp)
 (global-set-key (kbd "M-g") 'goto-line)
@@ -34,24 +28,10 @@
 (global-set-key [remap move-beginning-of-line] 'beginning-of-line-or-back-to-indention)
 (global-set-key (kbd "C-c C-c") 'comment-or-uncomment-region)
 (global-set-key [remap sh-case] 'comment-or-uncomment-region)
-(global-set-key (kbd "C-c r") 'comment-region)
-(global-set-key (kbd "C-c u") 'uncomment-region)
 
 ;; movement
 (global-set-key (kbd "C-c C-b") 'beginning-of-buffer)
 (global-set-key (kbd "C-c C-e") 'end-of-buffer)
-
-;; marking
-;; (global-set-key (kbd "C-<") 'mark-previous-like-this)
-;; (global-set-key (kbd "C->") 'mark-next-like-this)
-;; (global-set-key (kbd "C-M-m") 'mark-more-like-this)
-;; (global-set-key (kbd "C-*") 'mark-all-like-this)
-
-;; rcirc
-;;(global-set-key (kbd "C-c r c") 'rcirc) ;; reconnect...
-;;(global-set-key (kbd "C-c r u") 'rcirc-jump-to-first-unread-line)
-;;(global-set-key (kbd "C-c r s") 'mine-rcirc-shut-up)
-;;(global-set-key (kbd "C-c r t") 'mine-rcirc-talk-to-me)
 
 ;; docview mode
 (global-set-key (kbd "C-c n") 'doc-view-next-page)
@@ -85,10 +65,5 @@
 (global-set-key (kbd "C-c C-p") 'magit-push)
 (global-set-key (kbd "C-x C-l") 'magit-log)
 (global-set-key (kbd "C-x p") 'switch-to-magit-process)
-
-;; increment/decrement numbers
-(global-set-key (kbd "C-c +") 'mine-increment-decimal)
-(global-set-key (kbd "C-c =") 'mine-increment-decimal)
-(global-set-key (kbd "C-c -") 'mine-decrement-decimal)
 
 (provide 'mine-bindings)
