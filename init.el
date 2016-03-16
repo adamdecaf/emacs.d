38c462aad213d89d231d623ce4dd0888bd9f185d
Adam Shannon <adam@ashannon.us>
2016-03-16 09:26:28 -0500

    something happened to melpa

--- init.el
+++ init.el
@@ -21,7 +21,8 @@
 
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
                          ("marmalade" . "http://marmalade-repo.org/packages/")
-                         ("melpa" . "http://melpa.milkbox.net/packages/")))
+                         ;;("melpa" . "http://melpa.milkbox.net/packages/")
+			 ))
 
 (package-initialize)
 (package-refresh-contents)
