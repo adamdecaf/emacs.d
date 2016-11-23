0ae528c0bfc44d09b782e28de6318cb99f655b04
Adam Shannon <adamkshannon@gmail.com>
2016-11-23 11:31:55 -0600

    mine/org: Clean rss feeds

--- mine/mine-org.el
+++ mine/mine-org.el
@@ -49,9 +49,11 @@
 (setq org-feed-alist
       '((;; News
          "Al Jazeera"
-         "http://www.aljazeera.com/xml/rss/all.xml" "~/Dropbox/org/feeds.org" "Al Jazeera" :filter mine/org-feed-cleanse-cdata-tags)
+         "http://www.aljazeera.com/xml/rss/all.xml" "~/Dropbox/org/feeds.org" "Al Jazeera" :filter mine/org-feed-strip-html)
         ("BBC Top Stories"
-         "http://feeds.bbci.co.uk/news/rss.xml" "~/Dropbox/org/feeds.org" "BBC Top Stories" :filter mine/org-feed-cleanse-cdata-tags)
+         "http://feeds.bbci.co.uk/news/rss.xml" "~/Dropbox/org/feeds.org" "BBC Top Stories" :filter mine/org-feed-strip-html)
+        ("Des Moines Register"
+         "http://rssfeeds.desmoinesregister.com/desmoines/news&x=1" "~/Dropbox/org/feeds.org" "Des Moines Register" :filter mine/org-feed-strip-html)
 
         ;; no guids :(
         ;; ("FiveThirtyEight Politics"
@@ -61,21 +63,62 @@
 
         ;; Tech
         ("Hackernews" ;; https://news.ycombinator.com/rss doesn't have guid
-         "http://hnrss.org/newest?points=100" "~/Dropbox/org/feeds.org" "Hackernews" :filter mine/org-feed-cleanse-cdata-tags)
+         "http://hnrss.org/newest?points=100" "~/Dropbox/org/feeds.org" "Hackernews" :filter mine/org-feed-clean-hacker-news)
 
         ;; Weather
         ("DSM Weather"
-         "http://w1.weather.gov/xml/current_obs/KDSM.rss" "~/Dropbox/org/feeds.org" "DSM Weather" :filter mine/org-feed-cleanse-cdata-tags)
+         "http://w1.weather.gov/xml/current_obs/KDSM.rss" "~/Dropbox/org/feeds.org" "DSM Weather" :filter mine/org-feed-strip-html)
         ))
+
+
+(defun mine/strip-cdata (text)
+  (string-trim
+   (replace-regexp-in-string (regexp-quote "<![CDATA[") ""
+                             (replace-regexp-in-string (regexp-quote "]]>") "" text))))
 
 (defun mine/strip-html (text)
   (string-trim
-   (replace-regexp-in-string (regexp-quote "</?[a-zA-Z]*>") ""
-                             (replace-regexp-in-string (regexp-quote "<![CDATA[") ""
-                                                       (replace-regexp-in-string (regexp-quote "]]>") "" text)))))
+   (replace-regexp-in-string "<[a-zA-Z]*.*/>" ""
+                             (replace-regexp-in-string "<.*>.*</[a-zA-Z]*>" "" text))))
 
-(defun mine/org-feed-cleanse-cdata-tags(e)
-  (plist-put e :title (mine/strip-html (plist-get e :title)))
-  (plist-put e :description (mine/strip-html (plist-get e :description))))
+;; this is an org-feed filter
+(defun mine/org-feed-clean (f e)
+  (let* ((title (funcall f (plist-get e :title)))
+         (desc  (funcall f (plist-get e :description))))
+    (plist-put e :title title)
+    (plist-put e :description desc)))
+
+(defun mine/org-feed-cleanse-cdata-tags (e)
+  (mine/org-feed-clean mine/strip-cdata e))
+
+(defun mine/org-feed-strip-html (e)
+  (mine/org-feed-clean (lambda (e)
+                         (mine/strip-html
+                          (mine/strip-cdata e)))
+                       e))
+
+;; Hacker news custom filter
+;; Comments URL: <a href="https://news.ycombinator.com/item?id=13022663"
+(defun mine/org-feed-capture-comments-url (text)
+  (let* ((raw (when (string-match "Comments URL: <a href=\"\\(.*\\)\"" text)
+                (match-string 0 text))))
+    (if (not raw)
+        (setq raw ""))
+    (string-trim
+     (replace-regexp-in-string "\"?" ""
+                               (replace-regexp-in-string
+                                "URL: <a href=\""
+                                ""
+                                raw)
+                               ))))
+
+(defun mine/org-feed-clean-hacker-news (e)
+  (let* ((desc (plist-get e :description))
+         (comments (mine/org-feed-capture-comments-url desc))
+         (item (mine/org-feed-strip-html e)))
+    (message "%s" comments)
+    (if (and comments
+             (not (zerop (length comments))))
+        (plist-put item :description comments))))
 
 (provide 'mine-org)
