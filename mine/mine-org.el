4e6e8ee4a45009a20192a535a90c04e4190a0103
Adam Shannon <adamkshannon@gmail.com>
2017-09-13 12:43:45 -0500

    mine/org: clenaup (and remove feeds cleaning)

--- mine/mine-org.el
+++ mine/mine-org.el
@@ -4,7 +4,7 @@
 
 (setq org-log-done t)
 
-(setq org-directory "~/archive/org")
+(setq org-directory "~/archive")
 
 (setq org-todo-keywords
       '((sequence "BLOCKED(b)" "STARTED(s)" "TODO(t)" "NEXT(n)" "|" "DONE(d)" "CANCELLED(c)")))
@@ -17,65 +17,10 @@
         ("DONE" . "green")
         ("CANCELLED" . "gray")))
 
-;; org-feed-alist is read from ~/.emacs.d/hidden.el
-
 ;; Load up languages to use
 (org-babel-do-load-languages
  'org-babel-load-languages
  '((emacs-lisp . t)
    (sh . t)))
-
-(defun mine/strip-cdata (text)
-  (if (not text)
-      (setq text ""))
-  (string-trim
-   (replace-regexp-in-string (regexp-quote "<![CDATA[") ""
-                             (replace-regexp-in-string (regexp-quote "]]>") "" text))))
-
-(defun mine/strip-html (text)
-  (if (not text)
-      (setq text ""))
-  (string-trim
-   (replace-regexp-in-string "<[a-zA-Z]*.*/>" ""
-                             (replace-regexp-in-string "<.*>.*</[a-zA-Z]*>" "" text))))
-
-;; filter org-mode feeds
-(defun mine/org-feed-clean (f e)
-  (let* ((title (funcall f (plist-get e :title)))
-         (desc  (funcall f (plist-get e :description))))
-    (plist-put e :title title)
-    (plist-put e :description desc)))
-
-(defun mine/org-feed-cleanse-cdata-tags (e)
-  (mine/org-feed-clean mine/strip-cdata e))
-
-(defun mine/org-feed-strip-html (e)
-  (mine/org-feed-clean (lambda (e)
-                         (mine/strip-html
-                          (mine/strip-cdata e)))
-                       e))
-
-;; Hacker news custom filter
-(defun mine/org-feed-capture-comments-url (text)
-  (let* ((raw (when (string-match "Comments URL: <a href=\"\\(.*\\)\"" text)
-                (match-string 0 text))))
-    (if (not raw)
-        (setq raw ""))
-    (string-trim
-     (replace-regexp-in-string "\"?" ""
-                               (replace-regexp-in-string
-                                "URL: <a href=\""
-                                ""
-                                raw)
-                               ))))
-
-(defun mine/org-feed-clean-hacker-news (e)
-  (let* ((desc (plist-get e :description))
-         (comments (mine/org-feed-capture-comments-url desc))
-         (item (mine/org-feed-strip-html e)))
-    (message "%s" comments)
-    (if (and comments
-             (not (zerop (length comments))))
-        (plist-put item :description comments))))
 
 (provide 'mine-org)
