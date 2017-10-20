d4689b5c71db4046d739379387a4ebcc48e4cfba
Adam Shannon <adamkshannon@gmail.com>
2017-10-20 16:32:05 -0500

    better PATH force

--- mine/mine-env.el
+++ mine/mine-env.el
@@ -1,36 +1,20 @@
 ;; env steup
 (require 's)
 
-(defun mine/set-emacs-env-variable (name value)
-  (if (s-blank? (getenv name))
-      (setenv name value)))
+;; force PATH
+(setenv "PATH" (concat
+                "/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/usr/local/sbin"
+                ;; ":/Applications/Emacs.app/Contents/MacOS/bin-x86_64-10_9"
+                ;; ":/Applications/Emacs.app/Contents/MacOS/libexec-x86_64-10_9"
+                (concat ":" (getenv "HOME") "/src/banno/banno-deploy/bin")
+                (concat ":" (getenv "HOME") "/bin")
+                (concat ":" (getenv "GOPATH") "/bin")))
 
-(defun mine/update-path (incoming)
-  (if (not (s-contains? (getenv "PATH") incoming))
-      (setenv (concat (getenv "PATH") ":" incoming))))
-
-;; golang
-(if (eq system-type 'gnu/linux)
-    (setq gopath "/home/adam/src/go"))
-(if (eq system-type 'darwin)
-    (setq gopath "/Users/adam/src/go"))
-(mine/set-emacs-env-variable "GOPATH" gopath)
-(mine/update-path (concat gopath "/bin"))
-
-;; force path
-(when (not (string-match "local" (getenv "PATH")))
-  (setenv "PATH" (concat (getenv "PATH")
-                         ":/usr/local/bin:/usr/local/sbin")))
+;; (message (getenv "PATH"))
 
 (setenv "EDITOR" "emacs")
 (setenv "JAVA_HOME" "/Library/Java/JavaVirtualMachines/jdk1.8.0_60.jdk/Contents/Home")
 (setenv "TMPDIR" "/tmp") ;; tramp
 (setenv "TZ" "America/Chicago")
-
-;; docker
-(setenv "DOCKER_TLS_VERIFY" "1")
-(setenv "DOCKER_HOST" "tcp://192.168.99.100:2376")
-(setenv "DOCKER_CERT_PATH" "/Users/adam/.docker/machine/machines/banno-big")
-(setenv "DOCKER_MACHINE_NAME" "banno-big")
 
 (provide 'mine-env)
