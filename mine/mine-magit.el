850c9cfa75efc4badb800c3a6358b139f1ed7fea
Adam Shannon <adamkshannon@gmail.com>
2017-11-06 09:12:30 -0600

    mine/magit: don't show "recent commits"

--- mine/mine-magit.el
+++ mine/mine-magit.el
@@ -3,4 +3,7 @@
 ;; don't revert files for me magit.
 (setq magit-last-seen-setup-instructions "1.4.0")
 
+;; don't show "recent commits"
+(setq magit-log-section-commit-count 0)
+
 (provide 'mine-magit)
