8e698e31d526fe99c0a63e07e0efed487d665811
Adam Shannon <adamkshannon@gmail.com>
2017-12-13 11:02:30 -0600

    magit: only show 5 latest commits

--- mine/mine-magit.el
+++ mine/mine-magit.el
@@ -1,9 +1,6 @@
 (setq magit-push-always-verify nil)
 
-;; don't revert files for me magit.
-(setq magit-last-seen-setup-instructions "1.4.0")
-
 ;; don't show "recent commits"
-(setq magit-log-section-commit-count 0)
+(setq magit-log-section-commit-count 5)
 
 (provide 'mine-magit)
