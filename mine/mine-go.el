b8d00f5fe48837dd20b2e0df937ac5f2e79ded93
Adam Shannon <adamkshannon@gmail.com>
2018-10-22 12:09:15 -0500

    mine/go: support macOS vs linux godoc

--- mine/mine-go.el
+++ mine/mine-go.el
@@ -1,6 +1,9 @@
 ;; go-mode extensions and settings
 ;; see mine.el for before-save-hook
 
-(setq godoc-command "/usr/local/go/bin/godoc")
+(setq godoc-command
+      (if (eq system-type "darwin")
+          "/usr/local/go/bin/godoc"
+        "/usr/local/bin/godoc"))
 
 (provide 'mine-go)
