253c74c1d750364cddfdbe53525ec01dd3650917
Adam Shannon <adamkshannon@gmail.com>
2019-06-20 12:39:43 -0500

    mine/kubernetes: lower refresh rates
    Issue: https://github.com/chrisbarrett/kubernetes-el/issues/100#issuecomment-503684843

--- mine/mine-kubernetes.el
+++ mine/mine-kubernetes.el
@@ -10,4 +10,9 @@
       ;; From gcloud cli
       "/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/bin/kubectl")
 
+;; Lower refresh interval
+;; https://github.com/chrisbarrett/kubernetes-el/issues/100#issuecomment-503684843
+(setq kubernetes-poll-frequency 3600)
+(setq kubernetes-redraw-frequency 3600)
+
 (provide 'mine-kubernetes)
