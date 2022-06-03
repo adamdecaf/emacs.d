c1fd6d5c6c012b55dd3f97b1c173ecf53c149892
Adam Shannon <adamkshannon@gmail.com>
2022-06-03 08:42:32 -0500

    chore: update pinned certs

--- mine/mine-kubernetes.el
+++ mine/mine-kubernetes.el
@@ -1,10 +1,3 @@
-;; From https://github.com/bowei/kube.el
-;; (setq kube-kubectl-exec  "/usr/local/bin/kubectl")
-;;
-;; I had to add the following in kube--exec-command
-;; "--context" "k8s-banno-staging-context"
-
-
 ;; Settings for https://github.com/chrisbarrett/kubernetes-el
 (setq kubernetes-kubectl-executable
       ;; From gcloud cli
