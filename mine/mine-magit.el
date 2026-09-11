a0a821e0058f5728534febdc91be0e95dff6af06
Adam Shannon <adamkshannon@gmail.com>
2026-09-11 08:35:55 -0500

    mine: show worktrees on magit-status

--- mine/mine-magit.el
+++ mine/mine-magit.el
@@ -6,4 +6,8 @@
 ;;(add-hook 'magit-mode-hook 'turn-on-magit-gh-pulls)
 ;;(add-hook 'magit-mode-hook 'magit-todos-mode)
 
+(magit-add-section-hook 'magit-status-sections-hook
+                        #'magit-insert-worktrees
+                        nil t)
+
 (provide 'mine-magit)
